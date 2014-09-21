3d123d749a2a2a2fbb7d25dcc19a7067dbb961e1
Chenying <houchenying@gmail.com>
2014-09-21 16:42:49 -0400

    Add commnets to Classifier

--- src/edu/cmu/al/ml/Classifier.java
+++ src/edu/cmu/al/ml/Classifier.java
@@ -1,5 +1,20 @@
 package edu.cmu.al.ml;
-
+/**
+ * Classifier is the base class of all classifiers in training process. It is responsible of
+ * training labeled data and making prediction on all test data.                 
+ * @author chenying
+ *
+ */
 public abstract class Classifier {
+	/**
+	 * Train labeled data
+	 */
+	public abstract void train();
+	
+	/**
+	 * Make prediction on test data based on trained model and write prediction result back to 
+	 * database 
+	 */
+	public abstract void test();
 
 }
--- src/edu/cmu/al/ml/SVM.java
+++ src/edu/cmu/al/ml/SVM.java
@@ -2,4 +2,16 @@ package edu.cmu.al.ml;
 
 public class SVM extends Classifier {
 
+	@Override
+	public void train() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
