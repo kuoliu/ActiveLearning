b29f9852b2f5e072f1f9e0d2112fa55181282a7a
Youngyy <crystalyoungyy@gmail.com>
2014-09-22 08:56:33 -0400

    Add sampling comments

--- src/edu/cmu/al/sampling/BasicSampling.java
+++
@@ -1,5 +0,0 @@
-package edu.cmu.al.sampling;
-
-public class BasicSampling extends Sampling {
-
-}
--- src/edu/cmu/al/sampling/Sampling.java
+++ src/edu/cmu/al/sampling/Sampling.java
@@ -1,5 +1,11 @@
 package edu.cmu.al.sampling;
 
+/**
+ * Sampling is the base class of all sampling methods. It is responsible for
+ * providing some instances for user to label.
+ * @author yuanyuan
+ *
+ */
 public abstract class Sampling {
-
+	
 }
