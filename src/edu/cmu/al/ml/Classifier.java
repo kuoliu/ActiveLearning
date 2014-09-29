0f2920bbc3b5b161acbf9116550555720e5907b6
Chenying <houchenying@gmail.com>
2014-09-29 08:37:28 -0400

    Add part of classification part

--- src/edu/cmu/al/ml/Classifier.java
+++ src/edu/cmu/al/ml/Classifier.java
@@ -1,19 +1,24 @@
 package edu.cmu.al.ml;
+
 /**
- * Classifier is the base class of all classifiers in training process. It is responsible of
- * training labeled data and making prediction on all test data.                 
+ * Classifier is the base class of all classifiers in training process. It is
+ * responsible of training labeled data and making prediction on all test data.
+ * 
  * @author chenying
  *
  */
 public abstract class Classifier {
 	/**
-	 * Train labeled data
+	 * Train labeled data, it will first retrieve labeled training data from the data base, then 
+	 * train the data and build the model
+	 * @exception {@link ClassificationException}
 	 */
 	public abstract void train();
-	
+
 	/**
-	 * Make prediction on test data based on trained model and write prediction result back to 
-	 * database 
+	 * Make prediction on test data based on trained model and write prediction
+	 * result back to database
+	 * @exception {@link ClassificationException}
 	 */
 	public abstract void test();
 
