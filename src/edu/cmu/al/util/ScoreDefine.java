36f4e6267dd5722ce3ded0de1b5d44be14399545
zhengshuomm <zhengshuo0616@cmu.edu>
2014-10-13 13:55:58 -0400

    Add ExperimentResult class for doing experiment.

---
+++ src/edu/cmu/al/util/ScoreDefine.java
@@ -0,0 +1,33 @@
+package edu.cmu.al.util;
+
+public class ScoreDefine {
+	// positive instance means the review score is greater or equal to 4;
+	public static int posSocre = 4;      
+	
+	static int numberOfInstanceToLabel = 1000;
+	
+	public static int getNumberOfInstanceToLabel(double precision){
+		if (precision < 0.1){
+			return numberOfInstanceToLabel;
+		}else if (precision < 0.2){
+			return numberOfInstanceToLabel/2;
+		}else if (precision < 0.3){
+			return numberOfInstanceToLabel/3;
+		}else if (precision < 0.4){
+			return numberOfInstanceToLabel/4;
+		}else if (precision < 0.5){
+			return numberOfInstanceToLabel/5;
+		}else if (precision < 0.6){
+			return numberOfInstanceToLabel/6;
+		}else if (precision < 0.7){
+			return numberOfInstanceToLabel/7;
+		}else if (precision < 0.8){
+			return numberOfInstanceToLabel/8;
+		}else if (precision < 0.9){
+			return numberOfInstanceToLabel/9;
+		}else {
+			return numberOfInstanceToLabel/10;
+		}
+	}
+
+}
