51f4dd51a3c5e9d094c75d7bf3cd58add61e59f3
kuoliu <kuo.liu.pku@gmail.com>
2014-10-19 10:54:22 -0400

    1. Add corresponding testBasicSimulation in Unit test package, feel free to delete the testSimulation class. 2. format the code under simulation package to make the style the same.

--- src/edu/cmu/al/simulation/BasicLabelingSimulation.java
+++ src/edu/cmu/al/simulation/BasicLabelingSimulation.java
@@ -14,118 +14,124 @@ import edu.cmu.al.util.SqlManipulation;
 
 /**
  * @author zhengxiong
- *
+ * 
  */
 public class BasicLabelingSimulation implements LabelingSimulation {
 
-  // label the instance by product ID
-  @Override
-  public void labelProductId(Collection<String> productIds) {
-    if (productIds == null || productIds.size() == 0) {
-      return;
-    } // end if
+	// label the instance by product ID
+	@Override
+	public void labelProductId(Collection<String> productIds) {
+		if (productIds == null || productIds.size() == 0) {
+			return;
+		} // end if
 
-    for (String pId : productIds) {
-      String sql = "select avg(review_score) from " + Configuration.getReviewTable()
-              + " where product_id=?";
+		for (String pId : productIds) {
+			String sql = "select avg(review_score) from "
+					+ Configuration.getReviewTable() + " where product_id=?";
 
-      ResultSet rs = SqlManipulation.query(sql, pId);
-      String updateSql = "update " + Configuration.getPredictTable()
-              + " set (user_label, islabeled) = (?, ?) where product_id=?";
+			ResultSet rs = SqlManipulation.query(sql, pId);
+			String updateSql = "update "
+					+ Configuration.getPredictTable()
+					+ " set (user_label, islabeled) = (?, ?) where product_id=?";
 
-      try {
-        rs.next();
-        SqlManipulation.update(updateSql, Math.round(rs.getFloat(1)), true, pId);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    } // end for
-  }
+			try {
+				rs.next();
+				SqlManipulation.update(updateSql, Math.round(rs.getFloat(1)),
+						true, pId);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} // end for
+	}
 
-  // label all the instances
-  @Override
-  public void labelAll() {
-    List<String> productIds = getProductList();
-    labelProductId(productIds);
-  }
+	// label all the instances
+	@Override
+	public void labelAll() {
+		List<String> productIds = getProductList();
+		labelProductId(productIds);
+	}
 
-  // random label n instances
-  @Override
-  public List<String> randomLabelByNum(int n) {
-    List<String> productIds = getProductList();
+	// random label n instances
+	@Override
+	public List<String> randomLabelByNum(int n) {
+		List<String> productIds = getProductList();
 
-    if (n > productIds.size() || n <= 0) {
-      return null;
-    }
+		if (n > productIds.size() || n <= 0) {
+			return null;
+		}
 
-    shuffle(productIds);
-    List<String> newPIds = productIds.subList(0, n);
-    labelProductId(newPIds);
-    return newPIds;
-  }
+		shuffle(productIds);
+		List<String> newPIds = productIds.subList(0, n);
+		labelProductId(newPIds);
+		return newPIds;
+	}
 
-  // random label ration of intances
-  @Override
-  public List<String> randomLabelByRatio(float ratio) {
-    if (ratio < 0 || ratio > 1) {
-      return null;
-    }
+	// random label ration of intances
+	@Override
+	public List<String> randomLabelByRatio(float ratio) {
+		if (ratio < 0 || ratio > 1) {
+			return null;
+		}
 
-    List<String> productIds = getProductList();
-    shuffle(productIds);
-    List<String> newPIds = productIds.subList(0, Math.round(ratio * productIds.size()));
-    labelProductId(newPIds);
-    return newPIds;
-  }
+		List<String> productIds = getProductList();
+		shuffle(productIds);
+		List<String> newPIds = productIds.subList(0,
+				Math.round(ratio * productIds.size()));
+		labelProductId(newPIds);
+		return newPIds;
+	}
 
-  // get all instances in the database
-  /*
-   * private Set<String> getProductSet() { String sql = "select product_id from " +
-   * Configuration.getPredictTable(); ResultSet rs = SqlManipulation.query(sql);
-   * 
-   * Set<String> productIds = new HashSet<String>(); try { while (rs.next()) {
-   * productIds.add(rs.getString(1)); } } catch (SQLException e) { e.printStackTrace(); }
-   * 
-   * return productIds; }
-   */
+	// get all instances in the database
+	/*
+	 * private Set<String> getProductSet() { String sql =
+	 * "select product_id from " + Configuration.getPredictTable(); ResultSet rs
+	 * = SqlManipulation.query(sql);
+	 * 
+	 * Set<String> productIds = new HashSet<String>(); try { while (rs.next()) {
+	 * productIds.add(rs.getString(1)); } } catch (SQLException e) {
+	 * e.printStackTrace(); }
+	 * 
+	 * return productIds; }
+	 */
 
-  // get all instances in the database
-  private List<String> getProductList() {
-    String sql = "select product_id from " + Configuration.getPredictTable();
-    ResultSet rs = SqlManipulation.query(sql);
+	// get all instances in the database
+	private List<String> getProductList() {
+		String sql = "select product_id from "
+				+ Configuration.getPredictTable();
+		ResultSet rs = SqlManipulation.query(sql);
 
-    List<String> productIds = new ArrayList<String>();
+		List<String> productIds = new ArrayList<String>();
 
-    try {
-      while (rs.next()) {
-        productIds.add(rs.getString(1));
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+		try {
+			while (rs.next()) {
+				productIds.add(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    return productIds;
-  }
+		return productIds;
+	}
 
-  // shuffle the product Id in the list
-  private void shuffle(List<String> productIds) {
-    if (productIds == null || productIds.size() == 0) {
-      return;
-    }
+	// shuffle the product Id in the list
+	private void shuffle(List<String> productIds) {
+		if (productIds == null || productIds.size() == 0) {
+			return;
+		}
 
-    int size = productIds.size();
-    Random randomGenerator = new Random();
+		int size = productIds.size();
+		Random randomGenerator = new Random();
 
-    for (int i = 0; i < size; i++) {
-      int randomInt = randomGenerator.nextInt(size);
-      swap(productIds, i, randomInt);
-    }
-  }
+		for (int i = 0; i < size; i++) {
+			int randomInt = randomGenerator.nextInt(size);
+			swap(productIds, i, randomInt);
+		}
+	}
 
-  // swap two productIds in the list
-  private void swap(List<String> productIds, int source, int target) {
-    String temp = productIds.get(source);
-    productIds.set(source, productIds.get(target));
-    productIds.set(target, temp);
-  }
+	// swap two productIds in the list
+	private void swap(List<String> productIds, int source, int target) {
+		String temp = productIds.get(source);
+		productIds.set(source, productIds.get(target));
+		productIds.set(target, temp);
+	}
 }
--- src/edu/cmu/al/simulation/LabelingSimulation.java
+++ src/edu/cmu/al/simulation/LabelingSimulation.java
@@ -7,17 +7,18 @@ import java.util.Set;
 /**
  * @author Shuo Zheng, Zhengxiong Zhang
  * 
- *         The LabelingInterface interface is used to select instances from the prediction result,
- *         label a certain number of instances and send back the result. The interface can simulate
- *         the user's behavior, and label instances.
+ *         The LabelingInterface interface is used to select instances from the
+ *         prediction result, label a certain number of instances and send back
+ *         the result. The interface can simulate the user's behavior, and label
+ *         instances.
  */
 public interface LabelingSimulation {
 
-  public void labelProductId(Collection<String> productIds);
-  
-  public void labelAll();
-  
-  public List<String> randomLabelByNum(int n);
-  
-  public List<String> randomLabelByRatio(float ratio);
+	public void labelProductId(Collection<String> productIds);
+
+	public void labelAll();
+
+	public List<String> randomLabelByNum(int n);
+
+	public List<String> randomLabelByRatio(float ratio);
 }
